605d8ad03e9a237f194b6628d50642706ece6746
David Duthie <dave.a.duthie@gmail.com>
2021-09-26 19:04:36 +0200

    add a portion of a parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,8 +1,10 @@
-use nom::bytes::complete::tag;
-use nom::character::complete::{alpha1, space0};
-use nom::multi::separated_list1;
-use nom::sequence::{delimited, terminated};
 use nom::IResult;
+use nom::branch::alt;
+use nom::bytes::complete::{tag, take_until};
+use nom::character::complete::{alpha1, space0};
+use nom::combinator::not;
+use nom::multi::{many0, separated_list1};
+use nom::sequence::{delimited, terminated};
 
 #[derive(PartialEq, Eq, PartialOrd, Ord, Debug)]
 pub enum MustacheToken<'a> {
@@ -21,8 +23,27 @@ fn lookup(input: &str) -> Result {
     Ok((rest, MustacheToken::Lookup(idents)))
 }
 
+fn not_lookup(input: &str) -> Result {
+    let (rest, text) = take_until("{{")(input)?;
+    Ok((rest, MustacheToken::Text(text)))
+}
+
+fn mustache_token(input: &str) -> Result {
+    let (rest, tok) = alt((lookup, not_lookup))(input)?;
+    Ok((rest, tok))
+}
+
 mod tests {
     use super::*;
+
+    #[test]
+    fn not_lookup_test() {
+        for (template, token) in vec![
+            ("abc", MustacheToken::Text("abc")),
+        ] {
+            assert_eq!(Ok(("", token)), not_lookup(template));
+        }
+    }
 
     #[test]
     fn lookup_test() {
