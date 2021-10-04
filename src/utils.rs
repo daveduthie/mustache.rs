964b546f69cd327367e2290c83669af40072c431
David Duthie <dave.a.duthie@gmail.com>
2021-10-04 17:57:29 +0200

    fix log macro

--- src/utils.rs
+++ src/utils.rs
@@ -1,11 +1,10 @@
 extern crate web_sys;
 
-pub(crate) use log;
-
-// A macro to provide `println!(..)`-style syntax for `console.log` logging.
+/// A macro to provide `println!(..)`-style syntax for `console.log` logging.
 macro_rules! log {
     ( $( $t:tt )* ) => {
         web_sys::console::log_1(&format!( $( $t )* ).into());
     }
 }
 
+pub(crate) use log;
