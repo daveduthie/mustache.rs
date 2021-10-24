6f35d4edc959a07f2d77ce0f8805e96b549fcd79
David Duthie <dave.a.duthie@gmail.com>
2021-10-24 20:07:17 +0200

    serialize a bigger payload

--- js/index.js
+++ js/index.js
@@ -1,50 +1,62 @@
-import { Mustache, set_context, init_panic_hook } from "../mustache/pkg/index.js";
-import { doMaths } from "./raw.js"
-
-doMaths();
-
-// init_panic_hook();
+import { Mustache, set_context } from "../mustache/pkg/index.js";
+import { difference } from "./raw.js";
 
 const templates = [
-    Mustache.new("this is the thing: {{ calc.b.c }}, more stuff here: {{ other.stuff }}"),
-    Mustache.new("this is x.y.z: {{ x.y.z }}, but I also want {{ calc.b.c }}"),
-    Mustache.new("this doesn't exist: {{ calc.dontexist }}"),
+  Mustache.new(
+    "this is the thing: {{ calc.b.c }}, more stuff here: {{ other.stuff }}"
+  ),
+  Mustache.new("this is x.y.z: {{ x.y.z }}, but I also want {{ calc.b.c }}"),
+  Mustache.new("this doesn't exist: {{ calc.dontexist }}"),
 ];
 
 const userCalcs = {
-    "b": function(_ctx) {
-        console.log("Fun b evaluated")
-        return { c: "bananas" }
-    },
-    "ifail": function(_ctx) {
-        throw new Error("I fail")
-    }
-}
+  b: function (_ctx) {
+    console.log("Fun b evaluated");
+    return { c: "bananas" };
+  },
+  ifail: function (_ctx) {
+    throw new Error("I fail");
+  },
+};
 
 const initial_ctx = {
-    other: { stuff: true },
-    x: { y: { z: 123 } }
-}
+  other: { stuff: true },
+  x: { y: { z: 123 } },
+};
 
 const calcPrefix = ["calc"];
 const calcResults = {};
 
-templates.map((m) => {
+const render = async () => {
+  templates.map((m) => {
     m.deps(calcPrefix).map((dep) => {
-        if (!calcResults[dep]) {
-            const fun = userCalcs[dep];
-            if (fun) {
-                const res = fun(initial_ctx);
-                calcResults[dep] = res;
-            }
-
+      console.log({ dep });
+      if (!calcResults[dep]) {
+        const fun = userCalcs[dep];
+        if (fun) {
+          const res = fun(initial_ctx);
+          calcResults[dep] = res;
         }
-    })
-})
+      }
+    });
+  });
 
-initial_ctx["calc"] = calcResults;
-set_context(initial_ctx);
+  initial_ctx["calc"] = calcResults;
+  set_context(initial_ctx);
 
-const rendered = templates.reduce((acc, m) => { return acc + m.render() + "\n" }, "")
-const app = document.getElementById("app");
-app.innerText = rendered;
+  let rendered = templates.reduce((acc, m) => {
+    return acc + m.render() + "\n";
+  }, "");
+
+  const big_json = await fetch("circlecistatus.json").then((req) => req.json());
+  let d = await difference({ a: 1123, b: 2, ...big_json });
+  let d2 = await difference({ a: 2, b: 1000, ...big_json });
+  rendered += JSON.stringify(d, null, 2);
+  rendered += "\n";
+  rendered += JSON.stringify(d2, null, 2);
+  rendered += "\n";
+  const app = document.getElementById("app");
+  app.innerText = rendered;
+};
+
+render();
--- js/raw.js
+++ js/raw.js
@@ -1,15 +1,25 @@
+const dec = new TextDecoder();
+const enc = new TextEncoder();
+
 class WASMWrapper {
-  constructor(instance) {
+  constructor({ instance }) {
     this.instance = instance;
-    this.decoder = new TextDecoder();
-    this.encoder = new TextEncoder();
   }
 
   invoke(funName, value) {
     const fun = this.instance.exports[funName];
+    window.performance.mark("start_ser");
     const valuePtr = this.copyStringToWasm(JSON.stringify(value));
-    const s = this.copyStringFromWasm(fun(valuePtr));
-    return JSON.parse(s);
+    window.performance.mark("finish_ser");
+    const retPtr = fun(valuePtr);
+    window.performance.mark("finish_fun");
+    const s = this.copyStringFromWasm(retPtr);
+    let ret = JSON.parse(s);
+    window.performance.mark("finish_deser");
+    window.performance.measure("ser_time", "start_ser", "finish_ser");
+    window.performance.measure("fun_time", "finish_ser", "finish_fun");
+    window.performance.measure("deser_time", "finish_fun", "finish_deser");
+    return ret;
   }
 
   copyStringFromWasm(jsInteropStr) {
@@ -19,13 +29,13 @@ class WASMWrapper {
       stringData(jsInteropStr),
       stringLen(jsInteropStr)
     );
-    return this.decoder.decode(buf);
+    return dec.decode(buf);
   }
 
   copyStringToWasm(jsString) {
     const { memory, stringPrepare, stringData } = this.instance.exports;
 
-    const encodedString = this.encoder.encode(jsString);
+    const encodedString = enc.encode(jsString);
 
     // Ask WASM code to allocate a string inside of the module's memory
     const wasmStr = stringPrepare(encodedString.length);
@@ -52,14 +62,23 @@ const loadModule = async (moduleName) => {
     const mod = await WebAssembly.instantiateStreaming(
       fetch(`${moduleName}.wasm`)
     );
-    modules[moduleName] = mod;
+    modules[moduleName] = new WASMWrapper(mod);
   }
-
-  return new WASMWrapper(modules[moduleName].instance);
+  return modules[moduleName];
 };
 
-export async function doMaths() {
-  const fib = await loadModule("fib_bg");
-  const result = fib.invoke("difference", { a: 1000, b: 200 });
-  console.log(JSON.stringify({ result }, null, 2));
+export async function invoke(moduleName, funName, value) {
+  window.performance.mark("start_loading_mod");
+  const mod = await loadModule(moduleName);
+  window.performance.mark("loaded_mod");
+  let ret = mod.invoke(funName, value);
+  window.performance.mark("finished_funcall");
+  window.performance.measure("load_time", "start_loading_mod", "loaded_mod");
+  window.performance.measure("fn_time", "loaded_mod", "finished_funcall");
+  return ret;
+}
+
+export async function difference(value) {
+  let ret = await invoke("fib_bg", "difference", value);
+  return ret;
 }
